a0712dd30131f9ec8af21fa02f59d21a52be0fb0
Obinna Enenya <enenyaobinna@gmail.com>
2022-12-12 17:56:36 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single numeber 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 28; num < 38; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
