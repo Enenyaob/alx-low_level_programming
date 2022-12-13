44edcd838ce5fc6c7de7af86b2c29c300a1c1171
Obinna Enenya <enenyaobinna@gmail.com>
2022-12-12 17:40:42 -0800

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - script prints excluding q an e alphabets in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lt[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (lt[
+		putchar(lt[i]);
+	}
+	putchar('\n');
+	return (0);
