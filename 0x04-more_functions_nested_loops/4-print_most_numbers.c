51dcd3c5968bdf6e0b9026f42259c1aade33c742
Obinna Enenya <enenyaobinna@gmail.com>
2022-12-15 18:47:58 -0800

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_most_numbers -  prints the numbers, from 0 to 9 except 2 and 4
+ * Return: Always 0 Success
  */
 void print_most_numbers(void)
 {
@@ -11,7 +12,7 @@ void print_most_numbers(void)
 	{
 		if (i != 50 && i != 52)
 		{
-			_putchar(i);
+		_putchar(i);
 		}
 		_putchar('\n');
 	}
