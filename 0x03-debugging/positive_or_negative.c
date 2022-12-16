0b962fff4a5de6ce5c523a377d34c65963cdadca
Obinna Enenya <enenyaobinna@gmail.com>
2022-12-15 17:19:47 -0800

    update task 1

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Checks if number is positive or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
+}
